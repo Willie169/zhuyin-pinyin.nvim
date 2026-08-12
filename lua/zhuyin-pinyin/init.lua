326d213953afadb26c333d5ca6fa0286d8d35713
Willie Shen <cg10250207@gmail.com>
2026-08-12 16:56:41 +0800

    2026-08-12T08:56:41+00:00

--- lua/zhuyin-pinyin/init.lua
+++ lua/zhuyin-pinyin/init.lua
@@ -469,9 +469,9 @@ function M.zhuyin_key_to_pinyin(str)
 end
 
 function M.transform_selection(func)
-    local start_row, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
-    local end_row, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
-	local lines = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
+	local start_row, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
+	local end_row, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
+	local lines = vim.api.nvim_buf_get_text(0, start_row - 1, start_col - 1, end_row - 1, end_col - 1, {})
 	local text = table.concat(lines, "\n")
 	local transformed, err = func(text)
 	if not transformed then
