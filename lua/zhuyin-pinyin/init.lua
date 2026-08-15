566a47a7577df3c5f18c68143206773c9c6da58e
Willie Shen <cg10250207@gmail.com>
2026-08-15 18:06:13 +0800

    2026-08-15T10:06:13+00:00

--- lua/zhuyin-pinyin/init.lua
+++ lua/zhuyin-pinyin/init.lua
@@ -471,13 +471,13 @@ end
 function M.transform_selection(func)
 	local start_row, start_col = unpack(vim.fn.getpos("'<"), 2, 3)
 	local end_row, end_col = unpack(vim.fn.getpos("'>"), 2, 3)
-	local lines = vim.api.nvim_buf_get_text(0, start_row, start_col - 1, end_row - 1, end_col - 1, {})
+	local lines = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})
 	local text = table.concat(lines, "\n")
 	local transformed, err = func(text)
 	if not transformed then
 		return nil, err
 	end
-	vim.api.nvim_buf_set_text(0, start_row, start_col - 1, end_row - 1, end_col - 1, { transformed })
+	vim.api.nvim_buf_set_text(0, start_row, start_col, end_row, end_col, { transformed })
 	return text
 end
 
